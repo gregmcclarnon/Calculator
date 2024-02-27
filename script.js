0e91d2f3623a9a9bd21b95d7df44617a7c99b982
Greg McClarnon <greg@splaatphoto.co.uk>
2024-02-27 16:29:45 +0000

    chore: set up html, css and js feat: created +, -, x and divide functions

---
+++ script.js
@@ -0,0 +1,45 @@
+// FUNCTIONS
+
+// ADD
+
+function add(a,b) {
+    return a + b;
+}
+
+// SUBTRACT
+
+function subtract(a,b) {
+    return a - b;
+}
+
+// MULTIPLY
+
+function multiply(a,b) {
+    return a * b;
+}
+
+// DIVIDE
+
+function divide(a,b) {
+    return a / b;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TESTING
+console.log(add(2,2));
+console.log(subtract(2,2));
+console.log(multiply(2,2));
+console.log(divide(2,2));
